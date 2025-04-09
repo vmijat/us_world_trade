b39a6a26c4ac2b5c8f169d9db9e59e0f5af91089
vmijat <vmijatovic@gmail.com>
2025-04-09 07:37:38 +0200

    data from specific sheets

--- R/us_china_trade_visualization.R
+++ R/us_china_trade_visualization.R
@@ -1,3 +1,34 @@
 # load libraries
 
 library(tidyverse)
+library(readxl)
+
+
+# get data from US Bureau of Economic Analysis (BEA)
+
+url = "https://www.bea.gov/sites/default/files/2025-03/trad-geo-time-series-0125.xlsx"
+download.file(url = url, 
+              "./data_raw/us_trading_per_country.xlsx",
+              mode = "wb")
+
+
+
+# get excel data
+
+df <- readxl::read_xlsx("./data_raw/us_trading_per_country.xlsx")
+
+
+# reading specific tabs
+# "Table 4" is export of goods from US
+# "Table 5" is import of goods to US
+
+export_from_us <- readxl::read_xlsx("./data_raw/us_trading_per_country.xlsx",
+                                    sheet = "Table 4",
+                                    skip = 4)
+
+import_into_us <- readxl::read_xlsx("./data_raw/us_trading_per_country.xlsx",
+                                    sheet = "Table 5",
+                                    skip = 4)
+
+
+
